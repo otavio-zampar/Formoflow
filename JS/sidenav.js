21814560078f3ecf7ef1f18a42c71a1697f7dcfc
Otávio Zampar <60893393+otavio-zampar@users.noreply.github.com>
2023-11-08 23:51:37 -0300

    calculadora funciona em mobile

--- JS/sidenav.js
+++ JS/sidenav.js
@@ -1,13 +1,13 @@
 function openCalc() {
   if(openCal == 0){
     document.getElementById("myCalc").style.left = "0%";
-    document.getElementById("btnMyCalc").style.left = "calc(40% - 2.5rem)";
+    document.getElementById("btnMyCalc").style.left = "calc(max(40vw, min(100vh, 40rem)) - 2.5rem)";
     document.getElementById("darker").style.backgroundColor = "#00000050";
     document.getElementById("darker").style.pointerEvents = "auto";
     document.getElementById("btnCalc").style.transform = "rotate(0deg)";
     openCal = 1;
   }else{
-    document.getElementById("myCalc").style.left = "-40%";
+    document.getElementById("myCalc").style.left = "min(-40vw, max(-100vh, -40rem))";
     document.getElementById("btnMyCalc").style.left= "-3.5rem";
     document.getElementById("darker").style.backgroundColor = "#00000000";
     document.getElementById("darker").style.pointerEvents = "none";
@@ -19,13 +19,13 @@ function openCalc() {
 function openDraw() {
   if(openDra == 0){
     document.getElementById("myDraw").style.left = "0%";
-    document.getElementById("btnMyDraw").style.left = "calc(70% - 2.5rem)";
+    document.getElementById("btnMyDraw").style.left = "calc(max(70%, 20rem) - 2.5rem)";
     document.getElementById("darker").style.backgroundColor = "#00000050";
     document.getElementById("darker").style.pointerEvents = "auto";
     document.getElementById("btnDraw").style.transform = "rotate(0deg)";
     openDra = 1;
   }else{
-    document.getElementById("myDraw").style.left = "-70%";
+    document.getElementById("myDraw").style.left = "min(-70%, -20rem)";
     document.getElementById("btnMyDraw").style.left= "-3.5rem";
     document.getElementById("darker").style.backgroundColor = "#00000000";
     document.getElementById("darker").style.pointerEvents = "none";
